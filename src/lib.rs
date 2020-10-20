f7b648df4127f16b810effac1a7b81b166b957af
evenorog <even@impetus.no>
2020-10-20 10:12:11 +0200

    Add more tests for joins

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ use unicode_segmentation::UnicodeSegmentation;
 /// ```
 /// assert_eq!(reword::name("(Even),Olsson&Rogstadkjærnet?"), "Even Olsson Rogstadkjærnet");
 /// ```
-pub fn name(s: &str) -> String {
+pub fn name<T: AsRef<str>>(s: T) -> String {
+    let s = s.as_ref();
     let mut name = String::with_capacity(s.len());
     let mut it = s.unicode_words();
     if let Some(w) = it.next() {
@@ -38,7 +39,8 @@ pub fn name(s: &str) -> String {
 /// ```
 /// assert_eq!(reword::name_with_limit("(Even),Olsson&Rogstadkjærnet?", 4), "EOR");
 /// ```
-pub fn name_with_limit(s: &str, limit: usize) -> String {
+pub fn name_with_limit<T: AsRef<str>>(s: T, limit: usize) -> String {
+    let s = s.as_ref();
     let mut name: Vec<&str> = s.unicode_words().collect();
     if name.is_empty() {
         return String::new();
@@ -79,7 +81,7 @@ pub fn name_with_limit(s: &str, limit: usize) -> String {
 /// assert_eq!(reword::or_join(&["a", "b"]), "a or b");
 /// assert_eq!(reword::or_join(&["a", "b", "c"]), "a, b, or c");
 /// ```
-pub fn or_join(v: &[impl AsRef<str>]) -> String {
+pub fn or_join<T: AsRef<str>>(v: &[T]) -> String {
     if v.len() < 3 {
         join(v, " or ")
     } else {
@@ -94,7 +96,7 @@ pub fn or_join(v: &[impl AsRef<str>]) -> String {
 /// assert_eq!(reword::and_join(&["a", "b"]), "a and b");
 /// assert_eq!(reword::and_join(&["a", "b", "c"]), "a, b, and c");
 /// ```
-pub fn and_join(v: &[impl AsRef<str>]) -> String {
+pub fn and_join<T: AsRef<str>>(v: &[T]) -> String {
     if v.len() < 3 {
         join(v, " and ")
     } else {
@@ -102,7 +104,7 @@ pub fn and_join(v: &[impl AsRef<str>]) -> String {
     }
 }
 
-fn join(v: &[impl AsRef<str>], sep: &str) -> String {
+fn join<T: AsRef<str>>(v: &[T], sep: &str) -> String {
     let mut s = String::with_capacity(v.len() * 16);
     if let Some((first, tail)) = v.split_first() {
         s.push_str(first.as_ref());
@@ -131,5 +133,19 @@ mod tests {
         assert_eq!(crate::name_with_limit(s, 2), "EO");
         assert_eq!(crate::name_with_limit(s, 1), "E");
         assert_eq!(crate::name_with_limit(s, 0), "");
+    }
+
+    #[test]
+    fn join() {
+        assert_eq!(crate::or_join::<&str>(&[]), "");
+        assert_eq!(crate::or_join::<&str>(&["a"]), "a");
+        assert_eq!(crate::or_join::<&str>(&["a", "b"]), "a or b");
+        assert_eq!(crate::or_join::<&str>(&["a", "b", "c"]), "a, b, or c");
+        assert_eq!(crate::or_join::<&str>(&["a", "b", "c", "d", "e"]), "a, b, c, d, or e");
+        assert_eq!(crate::and_join::<&str>(&[]), "");
+        assert_eq!(crate::and_join::<&str>(&["a"]), "a");
+        assert_eq!(crate::and_join::<&str>(&["a", "b"]), "a and b");
+        assert_eq!(crate::and_join::<&str>(&["a", "b", "c"]), "a, b, and c");
+        assert_eq!(crate::and_join::<&str>(&["a", "b", "c", "d", "e"]), "a, b, c, d, and e");
     }
 }
